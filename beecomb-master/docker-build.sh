350883c44c1b82850235d756f516104e14a1fb88
徐芳方 <Fangfang.Xu@geely.com>
2022-04-19 08:58:56 +0800

    docker

---
+++ beecomb-master/docker-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION=$1
+echo "target version:$VERSION"
+sudo docker build --build-arg VERSION="${VERSION}" -t icodegarden/beecomb-master-"${VERSION}" .
+
+#sudo docker login
+#sudo docker push icodegarden/beecomb-master-"${VERSION}"
